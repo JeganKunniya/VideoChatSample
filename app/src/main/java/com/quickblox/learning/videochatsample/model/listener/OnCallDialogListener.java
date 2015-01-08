f0ae9a81e5b91f4c41a1d5d245f3fda9b5c06820
JeganKunniya <jegan.kunniya@gmail.com>
2015-01-08 21:29:37 +0530

    Video Chat Sample Code commit

---
+++ app/src/main/java/com/quickblox/learning/videochatsample/model/listener/OnCallDialogListener.java
@@ -0,0 +1,7 @@
+package com.quickblox.learning.videochatsample.model.listener;
+
+public interface OnCallDialogListener {
+
+    public void onAcceptCallClick();
+    public void onRejectCallClick();
+}
